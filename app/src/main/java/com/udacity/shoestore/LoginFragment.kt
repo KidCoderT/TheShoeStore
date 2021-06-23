6ad9201ec08248a50b443e08c6415a7eea8f6333
Tejas <Tejas75O25@gmail.com>
2021-06-23 12:10:47 +0530

    created login page and added it to navigation

---
+++ app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -0,0 +1,28 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_login,
+            container,
+            false
+        )
+
+        return binding.root
+    }
+}
