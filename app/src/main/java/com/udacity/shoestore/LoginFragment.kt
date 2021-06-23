d50c5ad0858c23697116b5e024d3fdf3dcc5bd60
Tejas <Tejas75O25@gmail.com>
2021-06-23 14:10:00 +0530

    Connected Login to Welcome page

--- app/src/main/java/com/udacity/shoestore/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -22,6 +23,14 @@ class LoginFragment : Fragment() {
             container,
             false
         )
+
+        binding.loginButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+        }
+
+        binding.registerButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_loginFragment_to_welcomeFragment)
+        }
 
         return binding.root
     }
