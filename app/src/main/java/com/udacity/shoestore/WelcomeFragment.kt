a67e5a8443e316ef306069433f1616a6cbb66048
Tejas <Tejas75O25@gmail.com>
2021-06-23 12:59:29 +0530

    Created the welcome fragment and added the fragment to navigation

---
+++ app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
@@ -0,0 +1,27 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+    private lateinit var binding: FragmentWelcomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_welcome,
+            container,
+            false
+        )
+
+        return binding.root
+    }
+}
